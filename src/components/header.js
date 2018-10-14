a370416ec30a144aca6c984d8446fcdc2e1894ff
Kirill <KirillGrinev@gmail.com>
2018-10-14 21:19:20 +0300

    first commit

--- src/components/header.js
+++ src/components/header.js
@@ -4,6 +4,7 @@ import '../css/styles.css'
 class Header extends Component{
 
     state = {
+        title:'The keywords are:',
         keywords:"Hello!"
     };
 
@@ -20,6 +21,8 @@ class Header extends Component{
             <header>
                 <div className="logo">Logo</div>
                 <input type="text" onChange={this.inputChangeHandler}/>
+                <div>{this.state.title}</div>
+                <div>{this.state.keywords}</div>
             </header>
         )
     }
